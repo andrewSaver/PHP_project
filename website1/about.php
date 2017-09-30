15d1162e627c0a5a1b9ce370bd7809403ee80ee7
andrewSaver <andysaver@gmail.com>
2017-09-29 20:08:28 -0400

    includes

---
+++ website1/about.php
@@ -0,0 +1,4 @@
+<?php require 'includes/header.php'; ?>
+    <h1>About</h1>
+
+<?php require 'includes/footer.php'; ?>
---
+++ website1/contact.php
@@ -0,0 +1,4 @@
+<?php require_once 'includes/header.php'; ?>
+    <h1>Contact</h1>
+
+<?php require_once 'includes/footer.php'; ?>
